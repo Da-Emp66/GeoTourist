5522cc2e21f5a0fe954c43eacc5887ac7860b9aa
Da-Emp66 <asherbean10@gmail.com>
2023-10-20 13:54:46 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+cd geotourist
+
+npm install cesium crypto-browserify stream-browserify browserify-zlib \
+    assert stream-http https-browserify os-browserify url path-browserify \
+    querystring-es3
+
+mv ./node_modules/cesium/Build/Cesium ./public/cesium
